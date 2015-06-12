4efe2d5c24573c6060f4385a24c21082cee29200
Richard Ulrich <richi@paraeasy.ch>
2015-06-12 23:34:27 +0200

    tests working

--- Tests/RunCMake/CommandLineGraphviz/graphviz_no_filter/RunCMakeTest.cmake
+++ Tests/RunCMake/CommandLineGraphviz/graphviz_no_filter/RunCMakeTest.cmake
@@ -1,4 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../RunCMake.cmake)
 
-set(RunCMake_TEST_OPTIONS --graphviz=no_filter.dot)
+set(RunCMake_TEST_OPTIONS --graphviz=graphviz_no_filter.dot)
 run_cmake(graphviz_no_filter)
